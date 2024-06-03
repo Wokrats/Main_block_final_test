470a8fae73e483bb1557e44acf708005e1d3113e
Maksim Starkov <wokrats2@gmail.com>
2024-06-03 21:39:03 +0500

    Add Readme

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] StrArray1 = new string[5] {"Hello", "2", "world", ":-|", "Hi"};
+string[] StrArray2 = new string[StrArray1.Length];
+void CreateNewArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+CreateNewArray(StrArray1, StrArray2);
+PrintArray(StrArray2);
